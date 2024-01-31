2cfafc06dbcf4f50ff7eab59921f85b58dc57b5b
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-01 00:22:50 +0100

    Refactor Home screen to use FlatList for rendering user profiles

--- src/screens/HomeScreen/Home.jsx
+++ src/screens/HomeScreen/Home.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import CardProfile from "../../components/Cards/cardProfile";
-
+import { DATA } from '../../data/usersData';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
   return (
-    <View>
-      <CardProfile />
-    </View>
+
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <CardProfile props={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.flatListContent}
+      />
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  flatListContent: {
+    // alignItems: 'center',
+  },
+});
