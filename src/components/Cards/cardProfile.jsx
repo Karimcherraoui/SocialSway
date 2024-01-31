3757343a57c6be31b30ac02b1af95051c2308e21
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-01 00:22:36 +0100

    Refactor cardProfile component to use props

--- src/components/Cards/cardProfile.jsx
+++ src/components/Cards/cardProfile.jsx
@@ -1,74 +1,45 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
-import { DATA } from '../../data/usersData';
 
-
-export default function CardProfile() {
-  const profileData = DATA[0]; // Assuming you want to display the first item in the DATA array
-
+export default function CardProfile({ props }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>{profileData.name}</Text>
-      <Image source={{ uri: profileData.img }} style={styles.profileImage} />
-        <View style={styles.info}>
-      <Text style={styles.infoText}>{profileData.country}</Text>
-        <Text style={styles.infoText}>{profileData.photos.length}</Text>
-        </View>
-          
-
-
+      <Text style={styles.infoText}>{props.name}</Text>
+      <Image source={{ uri: props.img }} style={styles.profileImage} />
+      <View style={styles.info}>
+        <Text style={styles.infoText}>{props.country}</Text>
+        <Text style={styles.infoText}>{props.photos.length}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+  container: {
+    alignItems:"center",
     borderRadius: 20,
     marginVertical: 15,
     backgroundColor: '#fff',
     shadowColor: 'gray',
-    shadowOffset: { width: 0, height: 3 },
-    shadowOpacity: 0.2,
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.3,
     shadowRadius: 4,
-    },
+
+  },
   profileImage: {
-    width: "95%",
-    height: "70%",
+    width: "95%", // Adjusted to take full width
+    height: 500, // Set a specific height or adjust as needed
     resizeMode: 'cover',
     borderRadius: 20,
-
   },
-  photoContainer: {
-    marginVertical: 10,
-  },
-  name:{
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginVertical: 10,
-  },
-
-  info:{
-    display: 'flex',
+  info: {
+    width: '70%',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: '80%',
-    paddingHorizontal: 20,
-    marginVertical: 20,
-
+    padding: 10,
   },
-
-  infoText:{
+  infoText: {
     fontSize: 20,
-
-  },
-  
-  photoImage: {
-    width: 100,
-    height: 100,
-    resizeMode: 'cover',
-    borderRadius: 10,
+    marginVertical: 10,
   },
 });
