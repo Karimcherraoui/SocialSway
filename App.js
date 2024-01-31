6d629c3cac3d95869b4f8603aba7d89c7667b259
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-01-31 09:16:31 +0100

    Refactor navigation and styling in App.js

--- App.js
+++ App.js
@@ -1,20 +1,116 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Entypo } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import Home from "./src/screens/HomeScreen/Home";
+import Faq from "./src/screens/Faq/Faq";
+import { StyleSheet } from "react-native";
+// import Settings from "./src/screens/Settings/Settings"; // Import your Settings screen
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={{
+      headerShown: false,
+      tabBarStyle: {
+        backgroundColor: "#ff6666",
+        justifyContent: "center",
+        alignItems: "center",
+        borderTopWidth: 0,
+      },
+    }}
+  >
+    <Tab.Screen
+      options={{
+        tabBarLabelStyle: {
+          fontSize: 13,
+          color: "white",
+        },
+        tabBarIcon: () => <Entypo name="home" size={24} color="white" />,
+      }}
+      name="Accueil"
+      component={Home}
+    />
+    <Tab.Screen
+      options={{
+        tabBarLabelStyle: {
+          fontSize: 13,
+          color: "white",
+        },
+        tabBarIcon: () => <AntDesign name="like1" size={24} color="white" />,
+      }}
+      name="Selectione"
+      component={Faq}
+    />
+  </Tab.Navigator>
+);
+
+const App = () => (
+  <NavigationContainer>
+    <Drawer.Navigator>
+      <Drawer.Screen name="AccueilDrawer" component={TabNavigator} options={{ 
+        title: 'Accueil',
+        headerStyle: {
+          backgroundColor: '#ff6666',
+      }}} />
+      <Drawer.Screen name="Faq" component={Faq} />
+    </Drawer.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconSetting: {
+    position: "absolute",
+    top: 70,
+    right: 20,
+  },
+
+  header: {
+    flex: 1.7,
+  },
+  menu: {
+    flexDirection: "row",
+    position: "absolute",
+    top: 70,
+    left: 10,
+    gap: 30,
+    alignItems: "center",
+  },
+  accueil: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+  appBar: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  text: {
+    color: "white",
+  },
+  bar: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "50%",
   },
 });
