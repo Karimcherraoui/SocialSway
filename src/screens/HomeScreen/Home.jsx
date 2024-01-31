4198c7de6dcdee393cf88e0ff39ce6d08b64d350
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-01-31 09:17:11 +0100

    Add Faq and Home components

---
+++ src/screens/HomeScreen/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+import CardProfile from "../../components/Cards/cardProfile";
+
+
+export default function Home() {
+  return (
+    <View>
+      <CardProfile />
+    </View>
+  );
+}
