90d631a3eeaf42082976d03e9c12805d31f2ae5a
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-01 00:23:15 +0100

    Add settings modal and handle setting button press

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import React from "react";
+import React, { useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Entypo } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import Home from "./src/screens/HomeScreen/Home";
 import Faq from "./src/screens/Faq/Faq";
-import { StyleSheet } from "react-native";
-// import Settings from "./src/screens/Settings/Settings"; // Import your Settings screen
+import { Ionicons } from "@expo/vector-icons";
 
+import { StyleSheet } from "react-native";
+import ModalSetting from "./src/components/Modal/ModalSetting";
+// import Settings from "./src/screens/Settings/Settings"; // Import your Settings screen
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -51,21 +53,52 @@ const TabNavigator = () => (
   </Tab.Navigator>
 );
 
-const App = () => (
-  <NavigationContainer>
-    <Drawer.Navigator>
-      <Drawer.Screen name="AccueilDrawer" component={TabNavigator} options={{ 
-        title: 'Accueil',
-        headerStyle: {
-          backgroundColor: '#ff6666',
-      }}} />
-      <Drawer.Screen name="Faq" component={Faq} />
-    </Drawer.Navigator>
-  </NavigationContainer>
-);
+const App = () => {
+  const [visible, setVisible] = useState(false);
 
+  const handleSetting = () => {
+    // console.log("setting");
+    setVisible(true);
+  };
+
+  return (
+    <>
+      <ModalSetting visible={visible} setVisible={setVisible} />
+      <NavigationContainer>
+        <Drawer.Navigator
+          screenOptions={{
+            headerTintColor: "white",
+            headerTitleAlign: "left",
+            headerRight: () => (
+              <Ionicons
+                name="settings-sharp"
+                size={24}
+                color="white"
+                onPress={handleSetting}
+              />
+            ),
+            headerRightContainerStyle: {
+              marginEnd: 20,
+            },
+          }}
+        >
+          <Drawer.Screen
+            name="AccueilDrawer"
+            component={TabNavigator}
+            options={{
+              title: "Accueil",
+              headerStyle: {
+                backgroundColor: "#ff6666",
+              },
+            }}
+          />
+          <Drawer.Screen name="Faq" component={Faq} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
 export default App;
-
 
 const styles = StyleSheet.create({
   container: {
