222fee9d75a13baeb7922b643b735188e238559c
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-05 11:05:55 +0100

    Refactor navigation and import statements

--- App.js
+++ App.js
@@ -1,103 +1,46 @@
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React, { useState } from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { Entypo } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-import Home from "./src/screens/HomeScreen/Home";
-import Faq from "./src/screens/Faq/Faq";
-import { Ionicons } from "@expo/vector-icons";
-
-import { StyleSheet } from "react-native";
 import ModalSetting from "./src/components/Modal/ModalSetting";
-// import Settings from "./src/screens/Settings/Settings"; // Import your Settings screen
-const Drawer = createDrawerNavigator();
-const Tab = createBottomTabNavigator();
+import Profile from "./src/screens/profileScreen/Profile";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet } from "react-native";
+import { DrawerNavigator } from "./src/components/Navigations/DrawerNavigator";
+
 const Stack = createNativeStackNavigator();
 
-const TabNavigator = () => (
-  <Tab.Navigator
-    screenOptions={{
-      headerShown: false,
-      tabBarStyle: {
-        backgroundColor: "#ff6666",
-        justifyContent: "center",
-        alignItems: "center",
-        borderTopWidth: 0,
-      },
-    }}
-  >
-    <Tab.Screen
-      options={{
-        tabBarLabelStyle: {
-          fontSize: 13,
-          color: "white",
-        },
-        tabBarIcon: () => <Entypo name="home" size={24} color="white" />,
-      }}
-      name="Accueil"
-      component={Home}
-    />
-    <Tab.Screen
-      options={{
-        tabBarLabelStyle: {
-          fontSize: 13,
-          color: "white",
-        },
-        tabBarIcon: () => <AntDesign name="like1" size={24} color="white" />,
-      }}
-      name="Selectione"
-      component={Faq}
-    />
-  </Tab.Navigator>
-);
-
 const App = () => {
-  const [visible, setVisible] = useState(false);
-
-  const handleSetting = () => {
-    // console.log("setting");
-    setVisible(true);
-  };
 
   return (
-    <>
-      <ModalSetting visible={visible} setVisible={setVisible} />
+
       <NavigationContainer>
-        <Drawer.Navigator
-          screenOptions={{
-            headerTintColor: "white",
-            headerTitleAlign: "left",
-            headerRight: () => (
-              <Ionicons
-                name="settings-sharp"
-                size={24}
-                color="white"
-                onPress={handleSetting}
-              />
-            ),
-            headerRightContainerStyle: {
-              marginEnd: 20,
-            },
-          }}
-        >
-          <Drawer.Screen
-            name="AccueilDrawer"
-            component={TabNavigator}
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Go Back"
+            component={DrawerNavigator}
             options={{
-              title: "Accueil",
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              headerTitle: "Profile",
               headerStyle: {
                 backgroundColor: "#ff6666",
               },
+              headerTintColor: "white",
             }}
           />
-          <Drawer.Screen name="Faq" component={Faq} />
-        </Drawer.Navigator>
+        </Stack.Navigator>
       </NavigationContainer>
-    </>
   );
 };
+
 export default App;
 
 const styles = StyleSheet.create({
