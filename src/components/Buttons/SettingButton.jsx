b8bb3e49312c29040491b175c0fddebfe6bd83c1
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-05 11:06:03 +0100

    Add SettingButton component

---
+++ src/components/Buttons/SettingButton.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Ionicons } from "@expo/vector-icons";
+
+export default function SettingButton  ()  {
+  return (
+        <View>
+        <Ionicons name="settings-outline" size={24} color="white" 
+        />
+        </View>
+  )
+}
+
